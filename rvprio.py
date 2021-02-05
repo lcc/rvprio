48c2d6f4283fe22b5117669ff27aca3dc07e3dbc
Lucas Cabral <lcc2@cesar.org.br>
2021-02-05 02:12:34 -0300

    feat(CLI): add **very** basic CLI
    Add very basic CLI alongside the architectural documentation.

---
+++ rvprio.py
@@ -0,0 +1,17 @@
+"""
+Runtime Verification Priorizator
+Usage: rvprio [options]
+
+  -h --help         Show this screen.
+"""
+import docopt
+
+
+def main():
+    """ Main function for rvprio. """
+    pass
+
+
+if __name__ == "__main__":
+    arguments = docopt.docopt(__doc__, version="beta")
+    main()
