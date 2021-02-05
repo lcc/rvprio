921ff303b32885a752c4d5a3d4d4d004e9b0ce58
Lucas Cabral <lcc2@cesar.org.br>
2021-02-05 02:40:53 -0300

    feat(validator): add arg validation for the CLI

--- rvprio.py
+++ rvprio.py
@@ -3,15 +3,20 @@ Runtime Verification Priorizator
 Usage: rvprio [options]
 
   -h --help         Show this screen.
+  -k --kernel=rv    Violations to look for [default: javamop]
 """
 import docopt
 
 
-def main():
+def main(kernel):
     """ Main function for rvprio. """
     pass
 
 
 if __name__ == "__main__":
     arguments = docopt.docopt(__doc__, version="beta")
-    main()
+    cli_validator = cerberus.Validator(rvprio.validator.CLI_SCHEMA)
+    is_valid = cli_validator.validate(arguments)
+    if is_valid:
+        arguments = {k.strip("--"): v for (k, v) in arguments.items()}
+        main(**arguments)
---
+++ rvprio/validator.py
@@ -0,0 +1,15 @@
+""" Validates the command line arguments. """
+
+CLI_SCHEMA = {
+    "--help": {
+        "nullable": False,
+        "required": True,
+        "type": "boolean",
+    },
+    "--kernel": {
+        "nullable": False,
+        "required": True,
+        "type": "string",
+        "allowed": ["javamop"],
+    },
+}
